ef06928e1c442a6732da934637382302427813a7
b05902052 <b05902052@linux10.csie.ntu.edu.tw>
2018-09-25 11:11:40 +0800

    Add simple but safe calculate command

--- bot.py
+++ bot.py
@@ -2,7 +2,12 @@ import discord
 from discord.ext import commands
 
 import random as rd
+import signal
 import json
+import re
+
+def timeout_handler(signum, frame):
+    raise signal.ItimerError
 
 config_filename = 'config.json'
 with open(config_filename, 'r') as f:
@@ -12,7 +17,7 @@ token = config['token']
 
 bot = commands.Bot(
     command_prefix='?',
-    description='A cute, automated elf living in NTUGDRC.'
+    description='A cute, automated elf.'
 )
 
 @bot.event
@@ -36,28 +41,58 @@ async def greet(ctx):
 
 @bot.command()
 async def test(ctx):
-    print(ctx.guild)
+    print(ctx.message.command)
 
 @bot.command()
 async def ooxx(ctx):
     await ctx.send("""Start a tic-tac-toe game.\nIt's still under construction now.""")
 
 @bot.command()
+async def calculate(ctx):
+    formula = ' '.join(ctx.message.content.split(' ')[1:])
+    if re.compile(r'[^0-9+\-*/.()!=\^&|<> ]').search(formula):
+        ans_msg = "The formula must be consisted of only digits and operations '+-*/.()!=^&|<>'"
+    else:
+        try:
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.setitimer(signal.ITIMER_REAL, 0.001)
+            ans = str(eval(formula))
+        # except signal.ItimerError:
+        except Exception as e:
+            signal.setitimer(signal.ITIMER_REAL, 0)
+            signal.signal(signal.SIGALRM, signal.SIG_DFL)
+            if (isinstance(e, signal.ItimerError)) or \
+               (isinstance(e, MemoryError)):
+                ans_msg = 'The question is too difficult, I can\'t solve it. QQ'
+            else:
+                ans_msg = 'The question is a little strange...'
+        else:
+            signal.setitimer(signal.ITIMER_REAL, 0)
+            signal.signal(signal.SIGALRM, signal.SIG_DFL)
+            if len(ans) > 100:
+                ans_msg = 'I have discovered a truly marvelous answer of your question, \nwhich this margin is too narrow to contain.'
+            else:
+                ans_msg = ans
+
+    embed = discord.Embed(description=ans_msg, color=0xeee657)
+    await ctx.send(embed=embed)
+
+@bot.command()
 async def info(ctx):
     
     embed = discord.Embed(
-        title="NTUGDRC-ELF", 
-        description="A cute, automated elf living in NTUGDRC.", 
+        title="About Mercedes", 
+        description="A cute, automated elf living in %s." % (ctx.guild), 
         color=0xeee657
     )
     
     # give info about you here
-    embed.add_field(name="Author", value="kikokushijo")
+    embed.add_field(name="Author", value="Dashex#5966 / kikokushijo @ github")
     
     # Shows the number of servers the bot is member of.
     # embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
 
-    # give users a link to invite thsi bot to their server
+    # give users a link to invite this bot to their server
     # embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
 
     await ctx.send(embed=embed)
@@ -68,23 +103,29 @@ bot.remove_command('help')
 async def help(ctx):
 
     embed = discord.Embed(
-        title="NTUGDRC-ELF", 
-        description="A cute, automated elf living in NTUGDRC. List of commands are:", 
+        title="Commands of Mercedes", 
+        description="A cute, automated elf living in %s.\nList of commands are:" \
+                     % (ctx.guild), 
         color=0xeee657
     )
     embed.add_field(
         name="%sgreet" % bot.command_prefix, 
-        value="Gives a nice greet message", 
+        value="Gives a greet message", 
         inline=False
     )
     embed.add_field(
         name="%sinfo" % bot.command_prefix, 
-        value="Gives a little info about the bot", 
+        value="Gives my info", 
         inline=False
     )
     embed.add_field(
         name="%shelp" % bot.command_prefix, 
         value="Gives this message", 
+        inline=False
+    )
+    embed.add_field(
+        name="%scalculate <formula>" % bot.command_prefix, 
+        value="Help you calculate the formula", 
         inline=False
     )
 
